41d7e08ca230fa2d8c39ac813f65d9bef4d22dc6
xemlock <xemlock@gmail.com>
2015-09-27 15:51:11 +0200

    fixed argument handling

--- dist/phpcompat.php
+++ dist/phpcompat.php
@@ -19,14 +19,22 @@ if (!function_exists('array_column')) {
      */
     function array_column($input, $column_key, $index_key = null)
     {
-        switch (func_num_args()) {
-            case 0:
-                trigger_error('array_column() expects at least 2 parameters, 0 given', E_USER_WARNING);
-                return null;
+        $num_args = func_num_args();
     
-            case 1:
-                trigger_error('array_column() expects at least 2 parameters, 1 given', E_USER_WARNING);
-                return null;
+        if ($num_args > 3) {
+            trigger_error(
+                sprintf('array_column() expects at most 3 parameters, %d given', $num_args),
+                E_USER_WARNING
+            );
+            return null;
+        }
+    
+        if ($num_args < 2) {
+            trigger_error(
+                sprintf('array_column() expects at least 2 parameters, %d given', $num_args),
+                E_USER_WARNING
+            );
+            return null;
         }
     
         if (!is_array($input)) {
@@ -34,8 +42,19 @@ if (!function_exists('array_column')) {
             return null;
         }
     
+        if (null !== $column_key
+            && !is_int($column_key)
+            && !is_float($column_key)
+            && !is_string($column_key)
+            && !(is_object($column_key) && method_exists($column_key, '__toString'))
+        ) {
+            trigger_error('array_column(): The column key should be either a string or an integer', E_USER_WARNING);
+            return false;
+        }
+    
         if (null !== $index_key
             && !is_int($index_key)
+            && !is_float($index_key)
             && !is_string($index_key)
             && !(is_object($index_key) && method_exists($index_key, '__toString'))
         ) {
--- src/function.array_column.php
+++ src/function.array_column.php
@@ -11,14 +11,22 @@
      */
     function phpcompat_array_column($input, $column_key, $index_key = null)
     {
-        switch (func_num_args()) {
-            case 0:
-                trigger_error('array_column() expects at least 2 parameters, 0 given', E_USER_WARNING);
-                return null;
+        $num_args = func_num_args();
 
-            case 1:
-                trigger_error('array_column() expects at least 2 parameters, 1 given', E_USER_WARNING);
-                return null;
+        if ($num_args > 3) {
+            trigger_error(
+                sprintf('array_column() expects at most 3 parameters, %d given', $num_args),
+                E_USER_WARNING
+            );
+            return null;
+        }
+
+        if ($num_args < 2) {
+            trigger_error(
+                sprintf('array_column() expects at least 2 parameters, %d given', $num_args),
+                E_USER_WARNING
+            );
+            return null;
         }
 
         if (!is_array($input)) {
@@ -26,8 +34,19 @@
             return null;
         }
 
+        if (null !== $column_key
+            && !is_int($column_key)
+            && !is_float($column_key)
+            && !is_string($column_key)
+            && !(is_object($column_key) && method_exists($column_key, '__toString'))
+        ) {
+            trigger_error('array_column(): The column key should be either a string or an integer', E_USER_WARNING);
+            return false;
+        }
+
         if (null !== $index_key
             && !is_int($index_key)
+            && !is_float($index_key)
             && !is_string($index_key)
             && !(is_object($index_key) && method_exists($index_key, '__toString'))
         ) {
